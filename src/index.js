a75393c0af5591e04afce407bda4f514f0b090a0
James <jamonjoroge816@gmail.com>
2024-04-03 11:04:53 +0300

    taskLiter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form=document.querySelector("#create-task-form")
+  const tasks=document.querySelector("#tasks")
+  console.log(tasks)
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const taskDescription=document.querySelector("#new-task-description")
+    const taskValue =taskDescription.value
+    if(taskValue == ""){
+      alert("Please write a to do")
+    }
+    else{
+
+      const taskList =document.createElement("li");
+      const deleteButton =document.createElement('button')
+      taskList.textContent=taskValue
+      deleteButton.textContent='x'
+      tasks.appendChild(taskList)
+      taskList.appendChild(deleteButton)
+      deleteButton.addEventListener('click',()=>{
+        taskList.remove()
+
+      })
+    }
+    form.reset()
+
+  })
+
 });
